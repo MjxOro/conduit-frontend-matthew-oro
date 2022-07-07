f1b278890a93aa3f0034af1ff17c4816e0ff8ce4
Matthew Oro <MatthewEOro@gmail.com>
2022-07-07 19:58:39 -0400

    refactor: Create global tailwind variables for common colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'main-green': '#5cb85c',
+        'main-grey': '#f2f2f2',
+        'tag-color': '#f3f3f3'
+
+      }
+    },
   },
   plugins: [],
 };
