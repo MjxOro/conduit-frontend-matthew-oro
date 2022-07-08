10b8991b3f14c11b815de23c7d029460329e96b5
Matthew Oro <MatthewEOro@gmail.com>
2022-07-07 20:14:31 -0400

    refactor: Change hexcode color to custom color variable

--- src/components/Homepage/Banner.js
+++ src/components/Homepage/Banner.js
@@ -1,6 +1,6 @@
 function Banner() {
   return (
-    <section className="flex flex-col justify-center items-center bg-[#5cb85c] text-white h-32">
+    <section className="flex flex-col justify-center items-center bg-main-green text-white h-32">
       <h1 className="text-5xl m-2">conduit</h1>
       <p className="m-2">A place to share your knowledge.</p>
     </section>
--- src/components/Homepage/Topics.js
+++ src/components/Homepage/Topics.js
@@ -2,10 +2,10 @@ function Topics() {
   return (
     <article className="py-4 px-[25%]">
       <nav className="flex flex-col-reverse md:flex-row md:items-center md:justify-center ">
-        <div className="mb-4 border-b border-[#5cb85c] dark:border-[#f2f2f2] flex-grow">
+        <div className="mb-4 border-b border-main-green dark:border-main-grey flex-grow">
           <ul className="flex flex-wrap -mb-px text-sm font-medium text-center">
             <li className="mr-2">
-              <button className="inline-block p-4 rounded-t-lg border-b-2 border-[#5cb85c] text-[#5cb85c] hover:text-[#f2f2f2] hover:border-[#5cb85c]">
+              <button className="inline-block p-4 rounded-t-lg border-b-2 border-main-green text-main-green hover:text-main-green hover:border-main-green">
                 Global Feed
               </button>
             </li>
@@ -15,7 +15,7 @@ function Topics() {
           <input
             type="text"
             placeholder="Popular Tags"
-            className="bg-[#f3f3f3] border border-[#f2f2f2] text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2  dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-tag-color border border-main-grey text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2  dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
       </nav>
