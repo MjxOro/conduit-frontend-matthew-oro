f534f3f919c0abb3e2c4689a4e83b65b7ace86c5
Matthew Oro <MatthewEOro@gmail.com>
2022-07-09 10:40:07 -0400

    refactor: Create "PopularTags" component

--- src/components/Homepage/Feed.js
+++ src/components/Homepage/Feed.js
@@ -1,14 +1,12 @@
+import PopularTags from "./PopularTags";
+
 function Topics() {
   return (
     <article className="py-4 px-[25%]">
       <nav className="flex flex-col-reverse md:flex-row md:items-center md:justify-center ">
         <div className="mb-4 border-b border-main-green dark:border-main-grey flex-grow">
           <ul className="flex flex-wrap -mb-px text-sm font-medium text-center">
-            <li className="mr-2">
-              <button className="inline-block p-4 rounded-t-lg border-b-2 border-main-green text-main-green hover:text-main-green hover:border-main-green">
-                Global Feed
-              </button>
-            </li>
+            <PopularTags/>
           </ul>
         </div>
         <ul className="md:ml-6 text-grey-900">
---
+++ src/components/Homepage/PopularTags.js
@@ -0,0 +1,10 @@
+function PopularTags() {
+  return (
+    <li className="mr-2">
+      <button className="inline-block p-4 rounded-t-lg border-b-2 border-main-green text-main-green hover:text-main-green hover:border-main-green">
+        Global Feed
+      </button>
+    </li>
+  );
+}
+export default PopularTags;
