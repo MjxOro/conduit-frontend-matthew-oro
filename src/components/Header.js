d1e9e57f4aad67ae2c245b139fae68443ca331c7
Matthew Oro <MatthewEOro@gmail.com>
2022-07-10 16:24:53 -0400

    refactor: Change color of header links and add hover color.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ function Header(){
   return(
     <header className="flex justify-between px-[25%]">
       <h1 className="text-main-green">conduit</h1>
-      <nav className="text-main-grey">
-        <a href="#" className="ml-4">Home</a>
-        <a href="#" className="ml-4">Sign in</a>
-        <a href="#" className="ml-4">Sign up</a>
+      <nav className="text-grey-link">
+        <a href="#" className="hover:text-grey-link-hover ml-4">Home</a>
+        <a href="#" className="hover:text-grey-link-hover ml-4">Sign in</a>
+        <a href="#" className="hover:text-grey-link-hover ml-4">Sign up</a>
       </nav>
     </header>
   )
