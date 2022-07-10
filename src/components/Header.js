8aaa6caf3fbecd99cc2c1b19dc1002079179b764
Matthew Oro <MatthewEOro@gmail.com>
2022-07-10 16:58:37 -0400

    refactor: Update header-nav design to follow design.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 function Header(){
   return(
-    <header className="flex justify-between px-[25%]">
-      <h1 className="text-main-green">conduit</h1>
+    <header className="flex justify-between px-[25%] py-3">
+      <h1 className="text-main-green font-bold text-xl">conduit</h1>
       <nav className="text-grey-link">
-        <a href="#" className="hover:text-grey-link-hover ml-4">Home</a>
-        <a href="#" className="hover:text-grey-link-hover ml-4">Sign in</a>
-        <a href="#" className="hover:text-grey-link-hover ml-4">Sign up</a>
+        <a href="#" className="text-sm hover:text-grey-link-hover ml-4">Home</a>
+        <a href="#" className="text-sm hover:text-grey-link-hover ml-4">Sign in</a>
+        <a href="#" className="text-sm hover:text-grey-link-hover ml-4">Sign up</a>
       </nav>
     </header>
   )
