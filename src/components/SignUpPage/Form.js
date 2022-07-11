3120f5a34fc94badb4d40771b85ee9188d7f50a1
Matthew Oro <MatthewEOro@gmail.com>
2022-07-11 12:46:39 -0400

    refactor: Change size of input box.

--- src/components/SignUpPage/Form.js
+++ src/components/SignUpPage/Form.js
@@ -1,6 +1,6 @@
 function Form(){
   return(
-    <div className="w-[50%] flex flex-col justify-center items-center my-4" >
+    <div className="w-[30%] flex flex-col justify-center items-center my-4" >
       <input type="text" placeholder="Username"  className="bg-white border border-gray-300 text-grey-900 text-sm placeholder:text-placeholder-grey rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-5 mb-4 "/>
       <input type="text" placeholder="Email"  className="bg-white border border-gray-300 text-grey-900 text-sm placeholder:text-placeholder-grey rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-5 mb-4 "/>
       <input type="text" placeholder="Password"  className="bg-white border border-gray-300 text-grey-900 text-sm placeholder:text-placeholder-grey rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-5 mb-4 "/>
