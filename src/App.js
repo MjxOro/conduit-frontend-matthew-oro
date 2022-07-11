11bf2882b54b58eb94dbe5f7ea6ee6484b64c4c8
Matthew Oro <MatthewEOro@gmail.com>
2022-07-11 19:25:25 -0400

    refactor: Remove extra import statement.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import HomePage from './components/Homepage/HomePage';
-import SignUpPage from "./components/SignUpPage/SignUpPage";
 
 function App() {
   return <HomePage />;
