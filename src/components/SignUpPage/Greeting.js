acfd8d1ccaf677563ffee78ea24d3cc009ae0411
Matthew Oro <60503218+MjxOro@users.noreply.github.com>
2022-07-11 20:38:55 -0400

    Feature/sign up page (#7)
    * feat: Create banner component
    
    * feat: Create topic tabs and tag search UI component
    
    * refactor: Create global tailwind variables for common colors
    
    * refactor: Change react fragment to div
    
    * refactor: Change hexcode color to custom color variable
    
    * refactor: Change name of "Topic" component to "Feed"
    
    * refactor: Create article component to separate from feed component
    
    * refactor: Change tags display from input box into a list block
    
    * refactor: Create "PopularTags" component
    
    * refactor: Move "Article" component into "Feed" component.
    
    * refactor: Change HTML semantic from "article" to "div".
    
    * refactor: Move "Feed" component into "Banner" component.
    
    * style: Prettier format all modified files.
    
    * refactor: fix typo in "Feed" component.
    
    * refactor: move "Feed" and "PopularTags" components out of "Banner" component and into "HomePage" component.
    
    * refactor: remove extra padding.
    
    * feat: Create form component for sign up page.
    
    * feat: Create greeting component for signup page.
    
    * feat: Create sign-up page component.
    
    * refactor: Change size of input box.
    
    * refactor: Show sign-up page on dev web environment.
    
    * refactor: Change return statement to homepage on app.js.
    
    * refactor: Change div to form.
    
    * refactor: Create inputbox component.
    
    * refactor: Remove extra import statement.

---
+++ src/components/SignUpPage/Greeting.js
@@ -0,0 +1,9 @@
+function Greeting(){
+  return(
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-3xl my-4 md:text-6xl lg:text-12xl">Sign Up</h1>
+      <a href="#" className="text-main-green">Have an account?</a>
+    </div>
+  )
+}
+export default Greeting;
