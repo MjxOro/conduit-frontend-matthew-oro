1070eada1af00235be35913ee666676e69a1cb71
Matthew Oro <MatthewEOro@gmail.com>
2022-07-13 20:07:08 -0400

    refactor: greeting component take 3 params.

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,8 @@
-function Greeting({title,link}){
+function Greeting({title,linkMessage,link}){
   return(
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-3xl my-4 md:text-6xl lg:text-12xl">{title}</h1>
-      <a href="#" className="text-main-green">{link}</a>
+      <a href={link} className="text-main-green">{linkMessage}</a>
     </div>
   )
 }
