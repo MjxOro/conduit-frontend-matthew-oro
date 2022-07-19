33dc9c93e852238e76170c47de71ad26322ef7c7
Matthew Oro <MatthewEOro@gmail.com>
2022-07-18 21:33:42 -0400

    chore: authform take more parameters for signup functionality.

--- src/components/AuthenticationForm.js
+++ src/components/AuthenticationForm.js
@@ -1,10 +1,10 @@
 import InputBox from "./InputBox";
 import GreenButton from "./GreenButton";
 
-function AuthenticationForm({ placeholders, name, buttonMessage, submitCallbackFn, }){
+function AuthenticationForm({ placeholders, name, type, buttonMessage, submitCallbackFn, }){
   return(
     <form onSubmit={submitCallbackFn} className="w-[30%] flex flex-col justify-center items-center my-4" >
-      {placeholders.map((placeholder) => <InputBox placeholder={placeholder} name={name} />)}
+      {placeholders.map((placeholder, i) => <InputBox key={i} type={type[i]} placeholder={placeholder} name={name[i]} />)}
       <GreenButton message={buttonMessage} />
     </form>
   )
