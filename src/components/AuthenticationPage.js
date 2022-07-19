749b44be79a3c4a18dc7f8d090b02f0b47491687
Matthew Oro <MatthewEOro@gmail.com>
2022-07-19 09:34:41 -0400

    feat: save sign-up data in session storage.

--- src/components/AuthenticationPage.js
+++ src/components/AuthenticationPage.js
@@ -2,29 +2,48 @@ import Greeting from "./Greeting";
 import Header from "./Header";
 import AuthenticationForm from "./AuthenticationForm";
 
+const signup = {
+  title: "Sign Up",
+  linkMessage: "Have an account?",
+  link: "#",
+  placeholders: ["Username", "Email", "Password"],
+  name: ["username","email","password"],
+  inputType: ["text", "text", "password"],
+  buttonMessage: "Submit"
+}
+const signin = {
+  title: "Sign In",
+  linkMessage: "Need an account?",
+  link: "#",
+  placeholders: ["Email", "Password"],
+  name: ["email","password"],
+  inputType: ["text", "text"],
+  buttonMessage: "Sign in"
+}
+const userData = {}
+
 function AuthenticationPage({isSignin}){
-  const signup = {
-    title: "Sign Up",
-    linkMessage: "Have an account?",
-    link: "#",
-    placeholders: ["Username", "Email", "Password"],
-    buttonMessage: "Submit"
-  }
-  const signin = {
-    title: "Sign In",
-    linkMessage: "Need an account?",
-    link: "#",
-    placeholders: ["Email", "Password"],
-    buttonMessage: "Sign in"
-  }
   return (
     <div>
       <Header />
       <div className="flex flex-col justify-center items-center">
         <Greeting title={ isSignin ? signin.title : signup.title } linkMessage={ isSignin ? signin.linkMessage : signup.linkMessage } link={ isSignin ? signin.link : signup.link } />
-        <AuthenticationForm placeholders={ isSignin ? signin.placeholders : signup.placeholders } buttonMessage={ isSignin ? signin.buttonMessage : signup.buttonMessage } />
+        <AuthenticationForm submitCallbackFn={ isSignin ? handleSubmitSignIn : handleSubmitSignUp } inputType={isSignin ? signin.inputType : signup.inputType }  placeholders={ isSignin ? signin.placeholders : signup.placeholders } name={ isSignin ? signin.name : signup.name } buttonMessage={ isSignin ? signin.buttonMessage : signup.buttonMessage } />
       </div>
     </div>
   )
+}
+
+function handleSubmitSignIn(){
+}
+
+function handleSubmitSignUp(event){
+  event.preventDefault();
+  signup.name.forEach((inputName) => {
+    const input = event.target[inputName];
+    userData[inputName] = input.value;
+  })
+  sessionStorage.setItem("user", userData);
+
 }
 export default AuthenticationPage;
