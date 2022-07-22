5c1858de8e66a9940da137c640129a04d18f9c13
Matthew Oro <MatthewEOro@gmail.com>
2022-07-21 21:10:34 -0400

    refactor: Change component name from GreenButton to SubmitButton.

--- src/components/AuthenticationForm.js
+++ src/components/AuthenticationForm.js
@@ -1,11 +1,11 @@
 import InputBox from "./InputBox";
-import GreenButton from "./GreenButton";
+import SubmitButton from "./SubmitButton";
 
 function AuthenticationForm({ placeholders, name, inputType, buttonMessage, submitCallbackFn }){
   return(
     <form onSubmit={submitCallbackFn} className="w-[30%] flex flex-col justify-center items-center my-4" >
       {placeholders.map((placeholder, i) => <InputBox key={i} type={inputType[i]} placeholder={placeholder} name={name[i]} />)}
-      <GreenButton message={buttonMessage} />
+      <SubmitButton message={buttonMessage} />
     </form>
   )
 }
--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,6 +1,6 @@
-function GreenButton({message}){
+function SubmitButton({message}){
   return(
     <button type="submit" className="self-end focus:outline-none text-white bg-main-green hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-md text-sm px-5 py-2.5">{message}</button>
   )
 }
-export default GreenButton;
+export default SubmitButton;
