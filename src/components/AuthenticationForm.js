672fc29cbb0d522156d0f23ab7bbfb02c8df7636
Matthew Oro <MatthewEOro@gmail.com>
2022-07-22 11:09:15 -0400

    chore: prettier format code.

--- src/components/AuthenticationForm.js
+++ src/components/AuthenticationForm.js
@@ -1,17 +1,23 @@
 import InputBox from "./InputBox";
 import SubmitButton from "./SubmitButton";
 
-function AuthenticationForm({ inputBox, buttonMessage, submitCallbackFn }){
-  return(
-    <form onSubmit={submitCallbackFn} className="w-[30%] flex flex-col justify-center items-center my-4" >
-      {inputBox.map((input, i) => <InputBox key={i}  placeholder={input.placeholder} name={input.name} type={input.type}/>)}
+function AuthenticationForm({ inputBox, buttonMessage, submitCallbackFn }) {
+  return (
+    <form
+      onSubmit={submitCallbackFn}
+      className="w-[30%] flex flex-col justify-center items-center my-4"
+    >
+      {inputBox.map((input, i) => (
+        <InputBox
+          key={i}
+          placeholder={input.placeholder}
+          name={input.name}
+          type={input.type}
+        />
+      ))}
       <SubmitButton message={buttonMessage} />
     </form>
-  )
+  );
 }
 
 export default AuthenticationForm;
-
-
-
-
--- src/components/AuthenticationPage.js
+++ src/components/AuthenticationPage.js
@@ -1,78 +1,96 @@
 import Greeting from "./Greeting";
 import Header from "./Header";
 import AuthenticationForm from "./AuthenticationForm";
-import axios from 'axios';
+import axios from "axios";
 
 const signup = {
   title: "Sign Up",
   linkMessage: "Have an account?",
   link: "#",
-  inputBox: [{placeholder: "Username", name: "username", type: "text"}, {placeholder: "Email", name: "email", type: "email"}, {placeholder: "Password", name: "password", type: "password"}],
-  buttonMessage: "Submit"
-}
+  inputBox: [
+    { placeholder: "Username", name: "username", type: "text" },
+    { placeholder: "Email", name: "email", type: "email" },
+    { placeholder: "Password", name: "password", type: "password" },
+  ],
+  buttonMessage: "Submit",
+};
 const signin = {
   title: "Sign In",
   linkMessage: "Need an account?",
   link: "#",
-  inputBox: [{placeholder: "Email", name: "email", type: "email"}, {placeholder: "Password", name: "password", type: "password"}],
-  buttonMessage: "Sign in"
-}
+  inputBox: [
+    { placeholder: "Email", name: "email", type: "email" },
+    { placeholder: "Password", name: "password", type: "password" },
+  ],
+  buttonMessage: "Sign in",
+};
 
-function AuthenticationPage({isSignin}){
+function AuthenticationPage({ isSignin }) {
   return (
     <div>
       <Header />
-        {
-          isSignin ?
-          <div className="flex flex-col justify-center items-center">
-            <Greeting title={ signin.title } linkMessage={ signin.linkMessage } link={ signin.link } />
-            <AuthenticationForm submitCallbackFn={ handleSubmitSignIn } inputBox={ signin.inputBox } buttonMessage={ signin.buttonMessage } />
-          </div>
-          :
-          <div className="flex flex-col justify-center items-center">
-            <Greeting title={ signup.title } linkMessage={ signup.linkMessage } link={ signup.link } />
-            <AuthenticationForm submitCallbackFn={ handleSubmitSignUp } inputBox={ signup.inputBox } buttonMessage={ signup.buttonMessage } />
-          </div>
-        }
+      {isSignin ? (
+        <div className="flex flex-col justify-center items-center">
+          <Greeting
+            title={signin.title}
+            linkMessage={signin.linkMessage}
+            link={signin.link}
+          />
+          <AuthenticationForm
+            submitCallbackFn={handleSubmitSignIn}
+            inputBox={signin.inputBox}
+            buttonMessage={signin.buttonMessage}
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center">
+          <Greeting
+            title={signup.title}
+            linkMessage={signup.linkMessage}
+            link={signup.link}
+          />
+          <AuthenticationForm
+            submitCallbackFn={handleSubmitSignUp}
+            inputBox={signup.inputBox}
+            buttonMessage={signup.buttonMessage}
+          />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-function handleSubmitSignIn(){
-}
+function handleSubmitSignIn() {}
 
-async function handleSubmitSignUp(event){
-  try{
+async function handleSubmitSignUp(event) {
+  try {
     event.preventDefault();
     const input = event.target;
 
     const jwt = await axios({
-      method: 'post',
+      method: "post",
       url: "https://launchup-prisma.herokuapp.com/api/users",
       data: {
-        "user":{
-          "username": input['username'].value,
-          "email": input['email'].value,
-          "password": input['password'].value,
-        }
-      }
-    })
+        user: {
+          username: input["username"].value,
+          email: input["email"].value,
+          password: input["password"].value,
+        },
+      },
+    });
 
     sessionStorage.setItem("token", jwt.token);
 
-    input['username'].value = "";
-    input['email'].value = "";
-    input['password'].value = "";
-
-  } catch(e){
-    if(e.response.status == 422 ){
+    input["username"].value = "";
+    input["email"].value = "";
+    input["password"].value = "";
+  } catch (e) {
+    if (e.response.status == 422) {
       throw new Error({
-        "errors":{
-          "body": [
-            "can't be empty"
-          ]
-        }
-      })
+        errors: {
+          body: ["can't be empty"],
+        },
+      });
     }
   }
 }
