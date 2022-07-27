2018ea710f0bd2a8af85741737607febe9b12ccd
Matthew Oro <60503218+MjxOro@users.noreply.github.com>
2022-07-27 18:57:46 -0400

    feat: create navigation routing. (#12)
    * feat: Create banner component
    
    * feat: Create topic tabs and tag search UI component
    
    * refactor: Create global tailwind variables for common colors
    
    * refactor: Change react fragment to div
    
    * refactor: Change hexcode color to custom color variable
    
    * refactor: Change name of "Topic" component to "Feed"
    
    * refactor: Create article component to separate from feed component
    
    * refactor: Change tags display from input box into a list block
    
    * refactor: Create "PopularTags" component
    
    * refactor: Move "Article" component into "Feed" component.
    
    * refactor: Change HTML semantic from "article" to "div".
    
    * refactor: Move "Feed" component into "Banner" component.
    
    * style: Prettier format all modified files.
    
    * refactor: fix typo in "Feed" component.
    
    * refactor: move "Feed" and "PopularTags" components out of "Banner" component and into "HomePage" component.
    
    * refactor: remove extra padding.
    
    * feat: install react-router-dom.
    
    * feat: set-up react routing.
    
    * feat: create routes for auth pages.
    
    * refactor: change link tags into react-router-dom link router.
    
    * feat: redirect authpage to homepage when prompts are successful.
    
    * feat: create navigation for authpage sub heading.

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Greeting({ title, linkMessage, link }) {
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-3xl my-4 md:text-6xl lg:text-12xl">{title}</h1>
-      <a href={link} className="text-main-green">
+      <Link to={link} className="text-main-green">
         {linkMessage}
-      </a>
+      </Link>
     </div>
   );
 }
