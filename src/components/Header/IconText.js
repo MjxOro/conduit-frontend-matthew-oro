17d25f49d268f6f77e1ba1b951c46406f7ea11c2
Matthew Oro <MatthewEOro@gmail.com>
2022-07-29 01:35:56 -0400

    refactor: put variables in the correct properties.

--- src/components/Header/IconText.js
+++ src/components/Header/IconText.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function IconText({ iconComponent, text, link }) {
   return (
-    <Link to={text} className="text-sm hover:text-grey-link-hover ml-4 flex items-center">
+    <Link to={link} className="text-sm hover:text-grey-link-hover ml-4 flex items-center">
       {iconComponent}
-      <p>text</p>
+      <p>{text}</p>
     </Link>
   )
 
