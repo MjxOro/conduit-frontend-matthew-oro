c36715a2b5fc4720b3ccdfe1d0ca2463dadfbe0d
Matthew Oro <60503218+MjxOro@users.noreply.github.com>
2022-08-07 13:38:56 -0400

    feat: create new post page. (#16)
    * feat: Create banner component
    
    * feat: Create topic tabs and tag search UI component
    
    * refactor: Create global tailwind variables for common colors
    
    * refactor: Change react fragment to div
    
    * refactor: Change hexcode color to custom color variable
    
    * refactor: Change name of "Topic" component to "Feed"
    
    * refactor: Create article component to separate from feed component
    
    * refactor: Change tags display from input box into a list block
    
    * refactor: Create "PopularTags" component
    
    * refactor: Move "Article" component into "Feed" component.
    
    * refactor: Change HTML semantic from "article" to "div".
    
    * refactor: Move "Feed" component into "Banner" component.
    
    * style: Prettier format all modified files.
    
    * refactor: fix typo in "Feed" component.
    
    * refactor: move "Feed" and "PopularTags" components out of "Banner" component and into "HomePage" component.
    
    * refactor: remove extra padding.
    
    * feat: create removable tag ui component.
    
    * feat: create new post page.
    
    * chore: add more props to reusable components.
    
    * chore: add routing to new post page.

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,7 +1,8 @@
-function SubmitButton({ message }) {
+function SubmitButton({ message, type, onClick = undefined }) {
   return (
     <button
-      type="submit"
+      type={type}
+      onClick={onClick}
       className="self-end focus:outline-none text-white bg-main-green hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-md text-sm px-5 py-2.5"
     >
       {message}
