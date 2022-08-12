f98aa6916453bfb082f70f41fde902c1ceaffd7d
Matthew Oro <MatthewEOro@gmail.com>
2022-08-12 11:12:32 -0400

    chore: create banner button component.

---
+++ src/components/BannerButton.js
@@ -0,0 +1,13 @@
+function BannerButton({ message, onClick, icon }) {
+  return (
+    <button
+      type="button"
+      className="flex items-center self-end text-gray-400 hover:text-white border border-gray-400 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      onClick={onClick}
+    >
+      {icon}
+      <p>{message}</p>
+    </button>
+  );
+}
+export default BannerButton;
