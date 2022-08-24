02a9047a5a53ebf64713e5949c2bba7c3ba91467
Matthew Oro <MatthewEOro@gmail.com>
2022-08-24 12:28:02 -0400

    refactor: refactor to inline style conditional.

--- src/components/TagContainer.js
+++ src/components/TagContainer.js
@@ -1,24 +1,16 @@
 import TagIcon from "./TagIcon";
 
 function TagContainer({ tag, onClick = undefined, feed = false, unremovable = false }) {
+  const feedTagStyle = "flex border border-grey-link m-2 text-grey-link justify-between items-center text-xs sm:text-sm rounded-full px-3 py-1 leading-loose"
+  const createTagStyle = "flex m-2 text-white justify-between items-center text-xs sm:text-sm bg-gray-400 rounded-full px-3 py-1 font-bold leading-loose" 
   return (
     <div>
-    {
-      !feed ?
-      <span className="flex m-2 text-white justify-between items-center text-xs sm:text-sm bg-gray-400 rounded-full px-3 py-1 font-bold leading-loose">
+      <span className={feed ? feedTagStyle : createTagStyle}>
         { !unremovable && 
           <TagIcon tag={tag} onClick={onClick} />
         }
         <p className="px-2">{tag}</p>
       </span>
-      :
-      <span className="flex border border-grey-link m-2 text-grey-link justify-between items-center text-xs sm:text-sm rounded-full px-3 py-1 leading-loose">
-        { !unremovable && 
-          <TagIcon tag={tag} onClick={onClick} />
-        }
-        <p className="px-2">{tag}</p>
-      </span>
-    }
     </div>
   ); 
 }
