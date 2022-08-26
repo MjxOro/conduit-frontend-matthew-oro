42f6381505277b31130627147fe9c30f8c4362ee
Matthew Oro <MatthewEOro@gmail.com>
2022-08-26 14:33:09 -0400

    refactor: add pointer cursor on popular tags.

--- src/components/TagContainer.js
+++ src/components/TagContainer.js
@@ -4,7 +4,7 @@ function TagContainer({ tag, onClick = undefined, feed = false, unremovable = fa
   const feedTagStyle = "flex border border-grey-link m-2 text-grey-link justify-between items-center text-xs sm:text-sm rounded-full px-3 py-1 leading-loose"
   const createTagStyle = "flex m-2 text-white justify-between items-center text-xs sm:text-sm bg-gray-400 rounded-full px-3 py-1 font-bold leading-loose" 
   return (
-    <div onClick={filter} id={id}>
+    <div onClick={filter} id={id} className={filter && "cursor-pointer" }>
       <span className={feed ? feedTagStyle : createTagStyle}>
         { !unremovable && 
           <TagIcon tag={tag} onClick={onClick} />
