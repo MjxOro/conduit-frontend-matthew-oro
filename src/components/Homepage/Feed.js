f8f9219b632edd338619a45e965ac8125b2d0ca9
Matthew Oro <MatthewEOro@gmail.com>
2022-08-29 14:01:47 -0400

    refactor: add functionality to feed tab that also responds to popular tags.

--- src/components/Homepage/Feed.js
+++ src/components/Homepage/Feed.js
@@ -1,32 +1,61 @@
 import Article from "./Article";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getFeed } from "../../feature/article/articleSlice";
+import { getFeed, setTabChoice } from "../../feature/article/articleSlice";
+import { BsHash } from "react-icons/bs";
 
 function Feed() {
-  const { articles } = useSelector((state) => state.articles);
+  const { articles, filteredArticles, tabName, tabChoice } = useSelector((state) => state.articles);
+  const chosenTabStyle = `rounded-t-lg p-4 border-b-2 border-main-green text-main-green hover:text-main-green hover:border-main-green`;
+  const idleTabStyle = `rounded-t-lg p-4 border-b-2 border-main-grey text-grey-link hover:text-main-green hover:border-main-green`;
   let dispatch = useDispatch();
   useEffect(() => {
     dispatch(getFeed());
   }, []);
+  function handleMap(article,index){
+    return(
+      <Article key={index} username={article.author.username} image={article.author.image} timeStamp={article.createdAt} likes={0} title={article.title} description={article.description} tags={article.tagList} />
+    )
+  }
+  function handleTabSwitch(choice){
+    switch(choice){
+      case 0:
+        return articles ? articles.map(handleMap) : "Nothing here yet..."
+      case 1:
+        return articles ? articles.map(handleMap) : "Nothing here yet..."
+      case 2:
+        return filteredArticles ? filteredArticles.map(handleMap) : "Nothing here yet..."
+      default:
+        return "Nothing here yet"
+        
+    }
+
+
+  }
   return (
     <div className="w-[75%]">
       <div className="mb-4 border-b border-main-green dark:border-main-grey flex-grow">
-        <ul className="flex flex-wrap -mb-px text-sm font-medium text-center">
-          <li className="mr-2">
-            <button className={`inline-block p-4 rounded-t-lg border-b-2 border-main-grey text-grey-link hover:text-main-green hover:border-main-green`}>
+        <div className="flex flex-wrap items-center -mb-px text-sm font-medium text-center">
+            <button onClick={() => dispatch(setTabChoice(0))} className={tabChoice === 0 ? chosenTabStyle : idleTabStyle}>
               Your Feed
             </button>
-            <button className="inline-block p-4 rounded-t-lg border-b-2 border-main-green text-main-green hover:text-main-green hover:border-main-green">
+            <button onClick={() => dispatch(setTabChoice(1))} className={tabChoice === 1 ? chosenTabStyle : idleTabStyle}>
               Global Feed
             </button>
-          </li>
-        </ul>
+            {
+              tabName &&
+              <button onClick={() => dispatch(setTabChoice(2))} className={tabChoice === 2 ? chosenTabStyle : idleTabStyle}>
+                <div className="flex items-center">
+                  <BsHash fontSize="1.25rem" />
+                  {tabName}
+                </div>
+              </button>
+            }
+        </div>
       </div>
       <div>
         {
-          articles ? articles?.map((article,index) => <Article key={index} username={article.author.username} image={article.author.image} timeStamp={article.createdAt} likes={0} title={article.title} description={article.description} tags={article.tagList} />) :
-          <p>No articles here... yet</p>
+          handleTabSwitch(tabChoice)
         }
       </div>
     </div>
